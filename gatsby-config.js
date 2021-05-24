03eaf01eea96dedef0d1e57510c445d1478fce42
Seshal Jain <seshaljain11@gmail.com>
2021-05-24 20:51:39 +0530

    Update environment variable config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,30 @@
-require("dotenv").config({
-  path: ".env",
+const { name, description, author, project } = require("./package.json")
+
+require("dotenv").config()
+
+const buildCredentials = ({
+  SPREADSHEET_ID,
+  PROJECT_ID,
+  PRIVATE_KEY,
+  PRIVATE_KEY_ID,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+}) => ({
+  spreadsheetId: SPREADSHEET_ID,
+  credentials: {
+    type: "service_account",
+    project_id: PROJECT_ID || project.id,
+    private_key_id: PRIVATE_KEY_ID || project.keyId,
+    private_key: (PRIVATE_KEY || project.key).replace(/(\\r)|(\\n)/g, "\n"),
+    client_email: CLIENT_EMAIL || project.email,
+    client_id: CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(
+      CLIENT_EMAIL || project.email
+    )}`,
+  },
 })
 
 module.exports = {
@@ -18,22 +43,7 @@ module.exports = {
     },
     {
       resolve: "gatsby-source-google-spreadsheets",
-      options: {
-        spreadsheetId: "1EIqFMAV_802bHx0gHoNtR6ujVliMLOb7wAo-4dG7yDg",
-        credentials: {
-          type: "service_account",
-          project_id: process.env.PROJECT_ID,
-          private_key_id: process.env.PRIVATE_KEY_ID,
-          private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, "\n"),
-          client_email: process.env.CLIENT_EMAIL,
-          client_id: process.env.CLIENT_ID,
-          auth_uri: "https://accounts.google.com/o/oauth2/auth",
-          token_uri: "https://oauth2.googleapis.com/token",
-          auth_provider_x509_cert_url:
-            "https://www.googleapis.com/oauth2/v1/certs",
-          client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.PROJECT_ID}%40appspot.gserviceaccount.com`,
-        },
-      },
+      options: buildCredentials(process.env),
     },
   ],
 }
